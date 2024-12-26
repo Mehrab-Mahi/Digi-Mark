2948c02be5389bac3ed69976a1c2cbaab2755d37
mehrab_mahi <mehrab.hosen@streamstech.com>
2024-12-26 14:37:12 +0600

    controller added

---
+++ Presentation/Controllers/User/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers.User;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    [HttpPost]
+    public ActionResult SignUp()
+    {
+        return Ok("okay");
+    }
+}
